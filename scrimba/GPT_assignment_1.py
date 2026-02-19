6a4eaab8e88f9d84639ea76ff45e30c9a1be49c4
HARSHVARDHAN-JOYBOY <bhusareharshvardhana2122004@gmail.com>
2026-02-19 09:17:44 +0530

    second commit updated GPT_assignment1

--- scrimba/GPT_assignment_1.py
+++ scrimba/GPT_assignment_1.py
@@ -22,20 +22,26 @@
 import string
 
 def clean(p1,p2):
-    p_one=p1.lower().translate(p1.maketrans("","",string.punctuation)).split()
-    p_two=p2.lower().translate(p2.maketrans("","",string.punctuation)).split()
+    p_one=p1.lower().translate(str.maketrans("","",string.punctuation)).split()
+    p_two=p2.lower().translate(str.maketrans("","",string.punctuation)).split()
     print(p_one,p_two)
     s1=set(p_one)
     s2=set(p_two)
-    print(f"{s1} \n {s2}")
-    print(f"Common words : {s1.intersection(s2)}")
+    # print(f"{s1} \n {s2}")
+    intsec=s1.intersection(s2)
+    print(f"Common words : {intsec}")
     print(f"Unique words : {s1.symmetric_difference(s2)}")
     freq=dict()
-    for word in zip(p_one,p_two):
+    for word in p_one+p_two:
         freq[word]=freq.get(word,0)+1
-
-
-
+    print(freq)
+    print(f"Words unique to paragraph 1: {s1-s2}")
+    print(f"Words unique to paragraph 2: {s2-s1}")
+    commonfreq=dict()
+    for word in p_one+p_two:
+        if word in intsec:
+            commonfreq[word]=commonfreq.get(word,0)+1
+    print(f"Common word frequency : {commonfreq.items()}")
 
 p1=input("Enter first paraghaph :")
 p2=input("Enter second paraghaph :")
